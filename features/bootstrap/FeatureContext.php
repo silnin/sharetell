e2361be2038f60012dca7f518379cffb734f0f20
Peter Keizer <peter@silnin.nl>
2016-11-17 23:59:01 +0100

    fixed test for registration

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,16 +1,53 @@
 <?php
 
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Silnin\UserBundle\Repository\UserRepository;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class FeatureContext extends MinkContext
+class FeatureContext extends MinkContext implements KernelAwareContext
 {
+    /** @var KernelInterface */
+    private $kernel;
+
+    /**
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @When /^I register "([^"]*)"$/
+     */
+    public function iRegister($email)
+    {
+        $this->visit('/register');
+        $this->fillField('fos_user_registration_form_email', $email);
+        $this->fillField('fos_user_registration_form_username', 'bddtest');
+        $this->fillField('fos_user_registration_form_plainPassword_first', 'test');
+        $this->fillField('fos_user_registration_form_plainPassword_second', 'test');
+        $this->pressButton('registration.submit');
+    }
+
+    /**
+     * @return \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected function getContainer()
+    {
+        return $this->kernel->getContainer();
+    }
 
     /**
      * @Given /^I am logged in$/
      */
     public function iAmLoggedIn()
     {
-        throw new \Behat\Behat\Tester\Exception\PendingException();
+        $this->visit('/logout');
+        $this->visit('/register');
+        $this->emailAddressIsAvailable('bddtest@silnin.nl');
+        $this->iRegister('bddtest@silnin.nl');
     }
 
     /**
@@ -19,5 +56,15 @@ class FeatureContext extends MinkContext
     public function thereIsIsAPublicStoryNamedAvailable($arg1)
     {
         throw new \Behat\Behat\Tester\Exception\PendingException();
+    }
+
+    /**
+     * @Given /^email address "([^"]*)" is available$/
+     */
+    public function emailAddressIsAvailable($emailAddress)
+    {
+        /** @var UserRepository $repo */
+        $repo = $this->kernel->getContainer()->get('silnin.user_repository');
+        $repo->deleteUserByEmail($emailAddress);
     }
 }
--- src/Silnin/ShareTell/DashboardBundle/Controller/DefaultController.php
+++ src/Silnin/ShareTell/DashboardBundle/Controller/DefaultController.php
@@ -2,12 +2,37 @@
 
 namespace Silnin\ShareTell\DashboardBundle\Controller;
 
+use Silnin\UserBundle\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 
 class DefaultController extends Controller
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var EngineInterface
+     */
+    private $twigEngine;
+
+    /**
+     * @param UserRepository $userRepository
+     * @param EngineInterface $twigEngine
+     */
+    public function __construct(UserRepository $userRepository, EngineInterface $twigEngine)
+    {
+        $this->userRepository = $userRepository;
+        $this->twigEngine = $twigEngine;
+    }
+
     public function indexAction()
     {
-        return $this->render('SilninShareTellDashboardBundle:Default:index.html.twig');
+        $params = [];
+        $params['username'] = $this->userRepository->getByEmail('gft_bak@hotmail.com')->getUsername();
+
+        return $this->twigEngine->renderResponse('SilninShareTellDashboardBundle:Default:index.html.twig', $params);
     }
 }
