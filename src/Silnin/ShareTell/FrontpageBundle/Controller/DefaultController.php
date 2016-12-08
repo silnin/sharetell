0650ca4ab63cf57e5d1d764bfe8cd7d17894bd60
Peter Keizer <peter@silnin.nl>
2016-12-08 20:24:44 +0100

    added test for app link verification

--- src/Silnin/ShareTell/FrontpageBundle/Controller/DefaultController.php
+++ src/Silnin/ShareTell/FrontpageBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Silnin\ShareTell\FrontpageBundle\Controller;
 use Silnin\ShareTell\StoryBundle\Repository\ContributionRepository;
 use Silnin\ShareTell\StoryBundle\Repository\ParticipantRepository;
 use Silnin\ShareTell\StoryBundle\Repository\StoryRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Templating\EngineInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -52,6 +53,22 @@ class DefaultController extends Controller
                 'Location' => 'http://' . $returnurl
             )
         );
+    }
+
+    public function assetLinksAction()
+    {
+        $headers = [];
+        $headers['Content-Type'] = 'application/json';
+
+        $content = [];
+        $content['relation'] = ["delegate_permission/common.handle_all_urls"];
+        $target = [];
+        $target['namespace'] = 'android_app';
+        $target['package_name'] = 'nl.silnin.sharetell';
+        $target['sha256_cert_fingerprints'] = ["C1:71:06:7D:1D:B9:05:09:89:CC:D2:57:46:3C:C9:0B:B9:23:A5:A3:FE:E0:B6:C0:5C:80:CF:00:CC:3F:57:EC"];
+        $content['target'] = $target;
+
+        return new JsonResponse([$content], 200, $headers);
     }
 
     public function readAction($reference)
